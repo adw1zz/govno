0be7a8023846da75667835e090f4d5109aa25207
adw1zz <vanae089@gmail.com>
2025-04-26 23:46:23 +0300

    sadasd

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-import Application from "./application.js";
-import cc from "cc";
+System.register(["./application.js"], function (_export, _context) {
 
-async function main() {
-  try {
-    const application = new Application();
-    const engine = await import(cc);
-    await application.init(engine);
-    await application.start();
-  } catch (err) {
-    console.error(err);
+  var Application, application;
+  function topLevelImport(url) {
+    return System["import"](url);
   }
-}
-
-main();
-
+  return {
+    setters: [function (_applicationJs) {
+      Application = _applicationJs.Application;
+    }],
+    execute: function () {
+      application = new Application();
+      topLevelImport('cc').then(function (engine) {
+        return application.init(engine);
+      }).then(function () {
+        return application.start();
+      })["catch"](function (err) {
+        console.error(err);
+      });
+    }
+  };
+});
